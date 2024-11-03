0c4b291b5d3043765c044f4ac307df24079fc7c6
Hubert Życiński <hzycinski@gmail.com>
2024-11-03 18:30:40 +0100

    Add test communication between back and front end

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,13 +1,17 @@
-import {app, BrowserWindow} from "electron";
-import path from "path"
+import { app, BrowserWindow } from "electron";
+import path from "path";
 import { isDevelopment } from "./util.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
-  if (isDevelopment()){
-    mainWindow.loadURL("http://localhost:5123")
-  }
-  else{
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
+  if (isDevelopment()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
-})
+});
---
+++ src/electron/pathResolver.ts
@@ -0,0 +1,11 @@
+import path from "path";
+import { app } from "electron";
+import { isDevelopment } from "./util.js";
+
+export function getPreloadPath() {
+  return path.join(
+    app.getAppPath(),
+    isDevelopment() ? "." : "..",
+    "/dist-electron/preload.cjs"
+  );
+}
